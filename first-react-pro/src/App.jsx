8cb2c6af8d592853862d62581e17bf2fe65ea1c5
Ahil <ahilmakwana26@gmail.com>
2026-03-14 21:03:02 +0530

    initial commit.

---
+++ first-react-pro/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    {/* Fregments */}
+    <div>
+      
+      <h1>Hi this is AHIl</h1>
+      <h2>Learning Blender</h2>
+      <h3>Leaning Unity</h3>
+    </div>
+    <div>
+      <h1>Thsi is React js</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
